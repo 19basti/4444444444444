5883a0936d2d3d96bd19c5adb536ab9eec391f05
19basti <47391297+19basti@users.noreply.github.com>
2023-04-02 05:47:24 +0200

    2
    Signed-off-by: 19basti <47391297+19basti@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,22 +1,89 @@
-<!DOCTYPE html>
 <html>
   <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>To-Do List</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <style>
+      /* Add styling for the to-do list */
+      .todo-list {
+        width: 500px;
+        margin: auto;
+        font-family: Arial, sans-serif;
+      }
+
+      /* Style the task items */
+      .todo-list li {
+        background-color: #f9f9f9;
+        padding: 10px;
+        margin-bottom: 10px;
+        border-radius: 5px;
+        box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1);
+        list-style: none;
+      }
+
+      /* Style the task items as completed */
+      .todo-list .completed {
+        text-decoration: line-through;
+        color: #ddd;
+      }
+
+      /* Style the add task button */
+      .add-task-btn {
+        background-color: #333;
+        color: #fff;
+        padding: 10px;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        float: right;
+      }
+
+      /* Style the task input field */
+      .add-task-input {
+        padding: 10px;
+        border-radius: 5px;
+        border: none;
+        width: 70%;
+        float: left;
+        margin-right: 10px;
+      }
+
+      /* Clear the floats */
+      .clear-fix {
+        clear: both;
+      }
+    </style>
   </head>
   <body>
-    <h1>To-Do List</h1>
-    <form id="to-do-form">
-      <input type="text" id="task" placeholder="Add a task">
-      <button type="submit">Add</button>
-    </form>
-    <ul id="to-do-list"></ul>
-    <script src="app.js"></script>
-  </body>
-</html>
+    <div class="todo-list">
+      <h1>To-Do List</h1>
+      <input type="text" class="add-task-input" placeholder="Add Task">
+      <button class="add-task-btn">Add Task</button>
+      <ul>
+        <!-- The task items will be added here dynamically -->
+      </ul>
+    </div>
+    <script>
+      // Get the task input field and the add task button
+      const taskInput = document.querySelector(".add-task-input");
+      const addTaskBtn = document.querySelector(".add-task-btn");
 
+      // Get the task list
+      const taskList = document.querySelector("ul");
 
+      // Add a new task to the list
+      addTaskBtn.addEventListener("click", function() {
+        // Get the task name from the input field
+        const taskName = taskInput.value;
 
+        // Create a new task item
+        const taskItem = document.createElement("li");
+        taskItem.innerHTML = taskName;
+
+        // Add a delete button to the task item
+        const deleteBtn = document.createElement("button");
+        deleteBtn.innerHTML = "Delete";
+        deleteBtn.style.float = "right";
+        deleteBtn.addEventListener("click", function() {
+          taskList.removeChild(taskItem);
+        });
+        taskItem.appendChild(deleteBtn);
+
+       
